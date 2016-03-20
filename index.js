bf3aef951533fd441fd5b6d01aeea8e2b7525abc
Jeremy <jkarbows@gmail.com>
2016-03-20 06:23:34 -0400

    synth test

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
-var submono = require("./submono")
 var app = express()
 var port = process.env.PORT || 5000
 
@@ -15,9 +14,6 @@ console.log("http server listening on %d", port)
 var webSockets = [];
 var client = null;
 
-var audioContext = new AudioContext()
-var synth = new Monosynth(audioContext)
-
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
@@ -26,8 +22,6 @@ wss.on("connection", function(ws) {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)*/
   console.log("websocket connection open")
-
-  synth.start();
 
   ws.on("message", function(data, flags) {
     console.log(JSON.stringify(data))
