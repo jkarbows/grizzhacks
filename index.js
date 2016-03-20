af711cdaac0ddb990451afa5d8c60c3599b568eb
Jeremy <jkarbows@gmail.com>
2016-03-19 21:21:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+    console.log('posted');
+});
+
+io.on('connection', function(socket) {
+    console.log('someone connected');
+});
+
+http.listen(3000, function() {
+    console.log('listening to u like the nsa');
+});
