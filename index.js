e5b0117e3cd3ac1d11844892be14091a9e788e0d
Jeremy <jkarbows@gmail.com>
2016-03-20 08:24:33 -0400

    git

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  /*var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)*/
+
   console.log("websocket connection open")
 
   ws.on("message", function(data, flags) {
