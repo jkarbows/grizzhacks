76cdb5d702813d878105920749df830d4421bafe
Jeremy <jkarbows@gmail.com>
2016-03-20 06:02:47 -0400

    git

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var WebSocketServer = require("ws").Server
 var http = require("http")
 var express = require("express")
+var submono = require("submono")
 var app = express()
 var port = process.env.PORT || 5000
 
@@ -25,9 +26,6 @@ wss.on("connection", function(ws) {
 
   ws.on("message", function(data, flags) {
     console.log(JSON.stringify(data))
-    /*for(f in data) {
-      console.log(f);
-    }*/
     wss.broadcast(JSON.stringify(data))
     client.send(JSON.stringify(data))
     console.log("message broadcast")
